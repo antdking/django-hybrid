ee6bcbcde1ab0af0012173f87e315172ea9b663e
Anthony King <cybojenix@slimroms.org>
2018-04-02 11:44:30 +0100

    setup tox

--- django_properties/tests/test.py
+++ django_properties/tests/test.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 
 class StubTestCase(TestCase):
     def test_stub(self):
-        self.assertEqual(1, 0)
+        self.assertEqual(1, 1)
--- runtests.py
+++ runtests.py
@@ -8,11 +8,15 @@ import django
 from django.core.management import call_command
 
 
-def runtests(*test_args):
-    print(test_args)
+def runtests():
+    import django.test.utils
+    from django.conf import settings
     django.setup()
-    call_command('test')
+    runner_class = django.test.utils.get_runner(settings)
+    test_runner = runner_class(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(['django_properties'])
+    sys.exit(failures)
 
 
 if __name__ == '__main__':
-    runtests(*sys.argv[1:])
+    runtests()
