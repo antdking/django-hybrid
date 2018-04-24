a0ce6ec8c45cdb4821e67b0b98b0b63770b1da81
Anthony King <cybojenix@slimroms.org>
2018-04-24 17:23:27 +0100

    change to using travis matrix

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup
 
-setup()
+tests_require = [
+    "pytest-cov",
+    "pytest-django",
+    "pytest-factoryboy",
+    "pytest-mock",
+]
+
+
+setup(
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    }
+)
