42b88c06506c7bffed088655a09fefff357b8269
Anthony King <cybojenix@slimroms.org>
2018-05-02 20:40:17 +0100

    add tests for expressionwrapper
    note a failing test

---
+++ dj_hybrid/tests/expression_wrapper/wrapper/test_expressionwrapper.py
@@ -0,0 +1,22 @@
+from django.db.models import ExpressionWrapper, Value, IntegerField, CharField
+
+from .base import WrapperTestBase
+from .factory import WrapperStubFactory
+from .models import WrapperStubModel
+
+
+class BaseExpressionWrapperTest(WrapperTestBase):
+    model_class = WrapperStubModel
+    factory = WrapperStubFactory
+
+
+class TestStrToInt(BaseExpressionWrapperTest):
+    python_value = 20
+    expression = ExpressionWrapper(Value("20"), output_field=IntegerField())
+
+
+class TestIntToStr(BaseExpressionWrapperTest):
+    python_value = "20"
+    expression = ExpressionWrapper(Value(20), CharField())
+
+    # TODO: this test currently fails parity checks.
